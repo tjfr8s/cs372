4d4318b8cd139921ec58a69bee421f5f3426a033
tyler <tjfr8s@gmail.com>
2019-05-01 09:48:48 -0700

    add comments to chatserve.py

--- project1/chatserve.py
+++ project1/chatserve.py
@@ -1,10 +1,24 @@
+"""
+Author: Tyler Freitas
+Date: 05/01/2019
+Description: This program creates a chat server listening on the port specified
+as the first (and only) command line argument.
+"""
 import socket
-
-def get_server_port():
-    port_input = int(input('Choose a port to listen on: '))
-    return port_input 
+import sys
 
 def send_message(server_socket_connection, user_name):
+    """
+    Gets user input and sends it to the passed socket as a message
+    with the user's name attached.
+
+    @param  server_socket_connection    target socket connection.
+    @param  user_name                   user name string to be added to 
+                                        messages.
+
+    @return                             returns true if the user quits and
+                                        false otherwise.
+    """
     message = input(f'{user_name}> ')
     if not message == '\quit':
         message_with_handle = f'{user_name}> {message}'
@@ -17,6 +31,14 @@ def send_message(server_socket_connection, user_name):
         return False
 
 def exchange_messages(server_socket_connection, user_name):
+    """
+    Coordinates the exchange of messages through the socket.
+
+    @param  server_socket_connection    target socket connection.
+    @param  user_name                   user name string to be added to 
+                                        messages.
+
+    """
     while True:
         data = server_socket_connection.recv(1024).decode()
         if data == '\quit':
@@ -28,7 +50,15 @@ def exchange_messages(server_socket_connection, user_name):
             return 0 
 
 def create_socket(host, server_port, user_name):
+    """
+    Creates a socket, binds it to a port, and listens for connections.
 
+    @param  host            empty string to indicate that we should  bind to all 
+                            interfaces.
+    @param  server_port     port to bind socket to.
+    """
+
+    # Bind socket to target address and port
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((host, server_port))
         s.listen(1)
@@ -38,20 +68,19 @@ def create_socket(host, server_port, user_name):
 
         return 
 
-
-def handle_requests(server_socket):
-    print('Handling requests...')
-    return 
-
 if __name__ == '__main__':
-    while(True):
-        # Request port from user.
-        SERVER_PORT = get_server_port() 
-        HOST = ''
+    if(len(sys.argv) < 2):
+        print("You did not specify a port")
 
-        user_name = input('Please enter your username: ')
+    else:
+        while(True):
+            # Request port from user.
+            SERVER_PORT = int(sys.argv[1])
+            HOST = ''
 
-        # Start server on specified port.
-        print('Started message server on port %d...' % SERVER_PORT)
-        create_socket(HOST, SERVER_PORT, user_name)
+            user_name = input('Please enter your username: ')
+
+            # Start server on specified port.
+            print('Started message server on port %d...' % SERVER_PORT)
+            create_socket(HOST, SERVER_PORT, user_name)
     
