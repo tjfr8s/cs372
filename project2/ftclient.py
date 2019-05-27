58b55f324465d1b535f5efcf043c9b74675373d4
tyler <tjfr8s@gmail.com>
2019-05-27 11:11:29 -0700

    finish comments

--- project2/ftclient.py
+++ project2/ftclient.py
@@ -1,40 +1,58 @@
+#!/bin/python3
+
+# Author: Tyler Freitas
+# Last Modified: 05/27/2019
+# Program Name: ftclient.py
+# Description: This program implements a file transfer client to be used with
+# ftserver.c. It is capable of requesting the directory contents of the server
+# as well as file that reside in that directory.
 import socket
 import sys
 
 def send_command(control_socket, command, host, port):
+    """
+    Description: Handles the sending of a command to the server.
+
+    Preconditions: The function receives a socket for sending the command
+    as well as the command itself, and the host:port that describe the socket.
+
+    Post conditions: The passed command is sent to the server via the passed
+    socket. If the command is successful the funciton returns true, else
+    it returns false.
+    """
+    # Join command components into a string.
     control_socket.sendall(" ".join(command).encode())
+    # Send command and wait for response.
     response = control_socket.recv(1024)
     response = response.decode()
 
+    # Handle potential error responses.
     if response == "ok":
         return True
     else:
         print(f'{host}:{port} says {response}')
         return False;
 
-
-    # if response is error print it and return false
-    
-    # if response is sending directory contents or file return true
-
 def create_control_socket(host, port):
+    """
+    Description: This function builds a socket for the passed host, port
+
+    Preconditions: The user passes a host and port for the socket.
+
+    Postconditions: the function returns a socket described by the host:port
+    tuple.
+    """
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((host, port))
     return s
 
 def create_data_socket(host, server_port):
     """
-    Creates a socket, binds it to a port, and listens for connections.
+    Description: Creates a socket, binds it to a port, and listens for connections.
 
-    @param  host            empty string to indicate that we should  bind to all 
-                            interfaces.
-    @param  server_port     port to bind socket to.
+    Preconditions: host, port, and username are passed to function
 
-    @preconditions:     
-     - host, port, and username are passed to function
-
-    @postconditions:
-     - a socket is created and used to exchange messages.
+    Postconditions:  a socket is created and used to exchange messages.
     """
 
     # Bind socket to target address and port
@@ -47,19 +65,49 @@ def create_data_socket(host, server_port):
         return conn
 
 def recv_list_directory(conn):
+    """
+    Description: this function receives and prints the result of a request
+    for directory contents to the server.
+
+    Preconditions: a request for directory contents has been made to the server
+    and the connection on which it was requested is passed to this function.
+
+    Postconditions: the function receives the results and prints them.
+    """
     message = conn.recv(1024).decode();
     print(message)
     return True
 
 def recv_file(control_socket, conn, filename):
+    """
+    Description: this function receives the file request from the server. 
+    
+    Preconditions: a request for a file has been made to the server
+    and the connection on which it was requested is passed to this function
+    along with the control_socket and filename.
+
+    Postconditions: The function receives chunks of the requested file and
+    appends them to the output file in the current directory.
+    """
     filename = "./" + filename 
     with open(filename, "w") as ofile:
+        # Keep receiving file contents until the server says the entire file
+        # has been transferred.
         while (control_socket.recv(1024).decode() == 'ok'):
             ofile.write(conn.recv(1024).decode())
         print("File transfer complete.")
     return True
 
 def parse_args(argv):
+    """
+    Description: This function parses the commandline arguments, builds a control
+    socket, sends the command, and handles the response from the server.
+
+    Preconditions: Commandline arguments are passed to the function.
+    
+    Postconditions: The passed command is sent to the server and the results
+    of that command are handled and an error is displayed if it wasn't successful.
+    """
     DATA_HOST = ''   
     CONTROL_HOST = argv[1]
     CONTROL_PORT = int(argv[2])
